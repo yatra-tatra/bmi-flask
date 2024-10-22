01fa7acd1035a7de1f986dd467f8628ca8b13c19
Yasuro Tanigawa <y.tanigawa@fujijoshi.ac.jp>
2024-10-22 11:57:33 +0900

    リポジトリへの登録

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# bmi-flask
+# BMI計算機をFlaskで
+## 概要
+
+- Pythonのプログラム・サンプルとして定番のBMI計算をFlaskを使ってwebアプリケーション化したもの
+- 教材としての使用を想定しており，いろいろ不完全な部分があります
+
+## 使用法
+
+1. ビルトインwebサーバの起動：`python3 app.py`（Linux, Mac），`py app.py`（Windows）
+2. ブラウザからアクセス：`http://localhost:5000/`
+3. 身長と体重を入力して「けいさん」ボタンを押すだけ
+4. サーバ停止で終了：`Ctrl-C`
+5. 以上
+
+## 参考資料
+### 書籍・Website
+
+- BMI計算プログラムの元ネタ1：森畑明昌『[Pythonによるプログラミング入門---東京大学教養学部テキスト---](https://www.utp.or.jp/book/b470446.html)』東京大学出版会, 2019.9.
+- [BMI計算プログラムの元ネタ2](https://gist.github.com/LouP-PV/65f3ff2ba2564e85a889787e993ba023)
+
+### CSSファイル
+
+staticフォルダ内のcssファイル（markdown7.css）は下記より拝借しております
+
+- https://github.com/sinnerschrader/markdown-css/tree/master
+- https://jasonm23.github.io/markdown-css-themes/
---
+++ app.py
@@ -0,0 +1,35 @@
+import sqlite3
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # データ入力ページの表示
+    return render_template('index.html')
+
+@app.route('/bmi', methods=["POST"])
+def bmi_post():
+    # 身長・体重データを取得
+    weight = float(request.form["in_weight"])
+    height = float(request.form["in_height"]) / 100
+
+    # BMI値を計算，結果の判定
+    bmi = round(weight / (height ** 2),1)
+    if bmi < 18.5:
+        result = "痩せ型"
+    elif (bmi >= 18.5) and (bmi < 25):
+        result = "標準体型"
+    elif (bmi >= 25) and (bmi < 30):
+        result = "肥満(軽)"
+    else:
+        result = "肥満(重)"
+        print("BMI: " + str(bmi))
+        print("判定: " + result)
+
+    # 結果ページの表示
+    return render_template('bmi.html', out_bmi = bmi, out_result = result)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost')
